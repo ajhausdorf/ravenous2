16c5e4b1f2193b5b5d26f3a6225f7ccec2b13ea7
ajhausdorf <ajhausdorf@wustl.edu>
2022-01-08 23:05:22 -0600

    Connect to the Yelp API

---
+++ src/util/Yelp.js
@@ -0,0 +1,35 @@
+const apiKey = 'wjqZ2OQkLq4kc493t8zc2uMR0eO3YvjVNNhRV79Ok-ckQwdhJv45Ahe0V55kWuK5Bb_Q4y-g0IrODxHZUi3aJihBqFR4lpXeDkdyxS9LaGsqOjkSCbrJOxJBUWPaYXYx';
+const clientId = 'HasVYc9erMB0Qs8CVC_pxw';
+
+export const Yelp = async (term, location, sortBy) => {
+    const endpoint = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`;
+    try {
+        const response = await fetch(endpoint, { 
+            headers: { 
+                Authorization: `Bearer ${apiKey}`
+            } 
+        });
+        if(response.ok){
+            const jsonResponse = await response.json();
+            if(jsonResponse.businesses) {
+                const businesses = jsonResponse.businesses.map(business => {
+                    return {
+                        id : business.id,
+                        imageSrc: business.image_url,
+                        name: business.name,
+                        address: business.address,
+                        city: business.city,
+                        state: business.state,
+                        zipCode: business.zipCode,
+                        category: business.category,
+                        rating: business.rating,
+                        reviewCount: business.reviewCount
+                    }
+                })
+                return businesses;
+            }
+        }
+    } catch(error) {
+        console.log(error);
+    }
+}
